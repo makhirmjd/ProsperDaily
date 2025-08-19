14604735014706789e2b3db910e7f180770bd40f
Muhammad Abdulmalik <makhirmjd@gmail.com>
2025-08-19 06:54:50 +0100

    Creating the statistics page

--- ProsperDaily/App.xaml.cs
+++ ProsperDaily/App.xaml.cs
@@ -7,18 +7,23 @@ namespace ProsperDaily
     {
         private readonly DashboardPageViewModel dashboardPageViewModel;
         private readonly TransactionPageViewModel transactionPageViewModel;
+        private readonly StatisticsPageViewModel statisticsPageViewModel;
 
-        public App(DashboardPageViewModel dashboardPageViewModel, TransactionPageViewModel transactionPageViewModel)
+        public App(DashboardPageViewModel dashboardPageViewModel, 
+            TransactionPageViewModel transactionPageViewModel,
+            StatisticsPageViewModel statisticsPageViewModel)
         {
             InitializeComponent();
             this.dashboardPageViewModel = dashboardPageViewModel;
             this.transactionPageViewModel = transactionPageViewModel;
+            this.statisticsPageViewModel = statisticsPageViewModel;
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
             //return new Window(new DashboardPageView(dashboardPageViewModel));
-            return new Window(new TransactionPageView(transactionPageViewModel));
+            //return new Window(new TransactionPageView(transactionPageViewModel));
+            return new Window(new StatisticsPageView(statisticsPageViewModel));
         }
     }
 }
